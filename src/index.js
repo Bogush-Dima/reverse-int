1dab198b7500d7331e9f1e633bc86b5874e2deba
Bogush-Dima <bogusd66@gmail.com>
2020-10-01 11:36:24 +0300

    feat: my decition this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const arr = (n + '').split('')
+  let result = 1;
+
+  if (arr[0] === '-') {
+    arr.shift()
+  }
+  result *= arr.reverse().join('')
+  return result
 }
